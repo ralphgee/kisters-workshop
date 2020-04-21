01ce907027284102a4510c48abe656385db0b7e3
asteffens <info@andreas-steffens.de>
2020-04-21 08:52:03 +0200

    inital commit

---
+++ src/test/java/de/as4it/workshops/spring/kisters/appcpntext/AppcpntextApplicationTests.java
@@ -0,0 +1,13 @@
+package de.as4it.workshops.spring.kisters.appcpntext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcpntextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
